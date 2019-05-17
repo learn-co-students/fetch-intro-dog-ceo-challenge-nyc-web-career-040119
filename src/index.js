f95d8799f24314c84cb1eb1d01d9a44a577ee579
Edgar Ong <emailedgar@gmail.com>
2019-05-17 10:04:38 -0400

    Challenge 1 complete

--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
-console.log('%c HI', 'color: firebrick')
+document.addEventListener('DOMContentLoaded', function(){
+
+  const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+  const dogsContainer = document.querySelector("#dog-image-container")
+
+  // FETCH IMAGES
+  fetch(imgUrl)
+    .then(function(response){
+      return response.json()
+    })
+    .then(function(dogImages){
+      const dogsUrl = dogImages.message
+
+      // add images to dogsContainer
+      dogsUrl.forEach(function(dog){
+        const newDog = document.createElement("img")
+        newDog.src = dog
+        dogsContainer.appendChild(newDog)
+      }) // end dogsUrl
+
+    }) // end dogImages
+}) // end DOMContentLoaded
